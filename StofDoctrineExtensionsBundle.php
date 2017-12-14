4c1323aafd3c4520bc9d8fe103c00e7d13e213dd
Christophe Coevoet <stof@notk.org>
2017-12-14 12:26:15 +0100

    Delay the static configuration of the file validator as late as possible
    Instead of doing it on boot, which adds a small overhead for all requests
    (even when the uploadable extension is not enabled at all), the static
    property is now initialized based on the config at the time the
    UploadableListener gets instantiated.

--- StofDoctrineExtensionsBundle.php
+++ StofDoctrineExtensionsBundle.php
@@ -6,7 +6,6 @@ use Stof\DoctrineExtensionsBundle\DependencyInjection\Compiler\SecurityContextPa
 use Stof\DoctrineExtensionsBundle\DependencyInjection\Compiler\ValidateExtensionConfigurationPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Gedmo\Uploadable\Mapping\Validator;
 
 class StofDoctrineExtensionsBundle extends Bundle
 {
@@ -17,12 +16,5 @@ class StofDoctrineExtensionsBundle extends Bundle
     {
         $container->addCompilerPass(new ValidateExtensionConfigurationPass());
         $container->addCompilerPass(new SecurityContextPass());
-    }
-
-    public function boot()
-    {
-        if ($this->container->hasParameter('stof_doctrine_extensions.uploadable.validate_writable_directory')) {
-            Validator::$validateWritableDirectory = $this->container->getParameter('stof_doctrine_extensions.uploadable.validate_writable_directory');
-        }
     }
 }
